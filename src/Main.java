01e5ed6e96c5a5b709b591ab578a6bd32f2a5630
AndreeaStejerean19 <andreeastejerean19@gmail.com>
2022-07-28 13:52:28 +0300

    Created assgn1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Exercitiul 1:");
+        System.out.println("Hello\nAndreea");
+
+        System.out.println("Exercitiul 2:");
+        System.out.println(1+2);
+
+        System.out.println("Exercitiul 3:");
+        System.out.println((float) 5/2);
+
+        System.out.println("Exercitiul 4:");
+        System.out.println("a." + ( -5 + 8 * 6));
+        System.out.println("b." + (55+9) % 9 );
+        System.out.println("c." + ( 20 + (float)-3*5 / 8));
+        System.out.println("d." + (5 + 15 / 3 * 2 - 8 % 3));
+    }
+}
